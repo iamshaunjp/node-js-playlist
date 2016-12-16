7f9ca7f063b4e253f5040ea85069067ab56dbef8
Shaik Saif Ali <saifshine7@gmail.com>
2016-12-17 04:02:26 +0530

    New line added.
    Writing into file command also added.

--- Practice/#10 File Read and Write/app.js
+++ Practice/#10 File Read and Write/app.js
@@ -6,3 +6,6 @@ var fs = require('fs');
 var sample = fs.readFileSync('sample.txt','utf8');
 // utf8 is encoding format| you can find clean explanation here at http://stackoverflow.com/a/15128103/5388823 
 console.log(sample);
+
+// this line of code creates an another file output.txt and writes the data in sample into the log.
+fs.writeFileSync('output.txt',sample);
