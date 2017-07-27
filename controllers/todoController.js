9dc435e40afe3c7937b16054c184fa4fa6e180f1
imvpn22 <imvpn22@gmail.com>
2017-07-27 23:21:08 +0530

    4th commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,27 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get milk'}, {item: 'Walk dog'}, {item: 'Do some code'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
 
   app.get('/todo', function(req, res){
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
-  app.post('/todo', function(req, res){
-
+  app.post('/todo', urlencodedParser, function(req, res){
+    data.push(req.body);
+    res.json(data);
   });
 
-  app.delete('/todo', function(req, res){
-
+  app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
   });
+
+
+
 
 };
