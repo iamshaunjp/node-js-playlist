cf95115b31295634367c2e42780e348f81766534
imvpn22 <imvpn22@gmail.com>
2017-07-27 21:07:46 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var app=express();
+var todoController = require('./controllers/todoController');
 
-var urlencoderParser = bodyParser.urlencoded({extended: false});
 
+var app = express();
+
+// Set up template engine
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('stuff'));
 
-app.get('/', function(req, res){
-  res.render('index');
-});
+// static files
+app.use(express.static('./public'));
 
-app.get('/contact', function(req, res){
-  res.render('contact', {qs: req.query});
-});
+// fire controllers
+todoController(app);
 
-app.post('/contact', urlencoderParser, function(req, res){
-  console.log(req.body);
-  res.render('contact-success', {data: req.body});
-});
-
-app.get('/profile/:name', function(req, res){
-  var data = {age:22, job:'net ninja', hobies:['singing', 'dancing', 'fishing']};
-  res.render('profile', {person: req.params.name, data: data});
-});
-
-app.listen(3000);
+// listen to port
+app.listen('3000');
+console.log('You are listening to port 3000');
---
+++ controllers/todoController.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+
+  app.get('/todo', function(req, res){
+
+  });
+
+  app.post('/todo', function(req, res){
+
+  });
+
+  app.delete('/todo', function(req, res){
+
+  });
+
+};
