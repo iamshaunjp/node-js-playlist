16336c3d43cb8096d92b021986899d860b6c350a
Abhijit Kumar <abhi110293@gmail.com>
2018-06-28 14:44:03 +0530

    Create part-1.js
    Created code(part-1) for - Node JS Tutorial for Beginners #16 - Pipes

---
+++ Practice/16 Pipe/part-1.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+myReadStream.pipe(myWriteStream); 
