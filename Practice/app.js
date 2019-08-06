369f845e3bb1d0fbb8400c3d616bfd4d10fa2ea0
rowlmars <49082023+rowlmars@users.noreply.github.com>
2019-08-05 19:40:03 -0700

    Tute27 partial templates

--- Practice/app.js
+++ Practice/app.js
@@ -6,11 +6,11 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
 app.get('/profile/:name', function(req, res){
